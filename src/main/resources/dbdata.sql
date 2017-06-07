9b7eb7780f49bd38377932f464a4a2970647d567
RojerAlone <1091165843@qq.com>
2017-06-07 11:44:08 +0800

    添加数据导入sql文件
    TODO：添加tagmapper表数据以及img表，复制图片到WEB-INF目录下

---
+++ src/main/resources/dbdata.sql
@@ -0,0 +1,241 @@
+insert into `shop`.`game`(`creater`, `name`, `desc`, `systemcfg`, `price`) values
+ ('Killerfish Games', 'Cold Waters', 'Spiritual Successor to the Microprose Classic “Red Storm Rising”.',
+  '操作系统: XP 处理器: Intel Atom
+  内存: 2 GB RAM
+  DirectX 版本: 9.0c
+  存储空间: 需要 2 GB 可用空间', 58),
+ ('LR Studio', 'Confess My Love', '游戏发生在某一天的放学后，男生未宇打算向心仪的女生阑珊表明自己的心意，但又因为害怕被拒绝犹豫不决。',
+  '操作系统: Windows XP/ Vista/7/10
+  处理器: Intel Core 2 1.06Ghz
+  内存: 1 GB RAM
+  存储空间: 需要 50 MB 可用空间', 0),
+ ('Motion Twin', 'Dead Cells', '欢迎来到《死亡细胞》，这是一款将Roguelite与银河战士恶魔城类特点融为一炉的2D平台动作游戏。游戏中并无检查点，玩家将体验魂味战斗，一路挑战诸多守卫，在杀戮与死亡的反复轮回中探索一座房间不断变化的巨大城堡。”.',
+  '操作系统: Windows 7+
+  处理器: Intel i5+
+  内存: 2 GB RAM
+  图形: Nvidia 450 GTS / Radeon HD 5750 or better
+  存储空间: 需要 500 MB 可用空间
+  附注事项: OpenGL 3.2+', 0),
+ ('ROCKFISH Games', 'EVERSPACE', '《EVERSPACE™》是一款侧重于动作的单人太空射击游戏，结合了类Rogue元素和一流的视觉效果及引人入胜的故事。游戏将带领你在一个不断变化、制作精美且充满惊喜的宇宙中，展开一段富有挑战性的旅程。 你的技能、经验和随机应变的天赋都将持续受到考验；与此同时，通过遭遇其他有趣且各有奥秘的人物角色，你将了解自己的存在意义。每个回合都是激动人心的体验，每次航行都是全新的局势，让游戏的每一个部分都能长久地持续下去， 并从中创造大量独特而意味深长的体验时刻。然而，不管你的飞行技术多么高超，死亡都是不可避免的，而这只是一段更宏大旅程的开始。',
+  '操作系统: 32/64-bit Windows 7 / 8.1 / 10
+  处理器: Intel CPU Core i3
+  内存: 4 GB RAM
+  图形: Nvidia GTX 480 / AMD Radeon HD 5870
+  DirectX 版本: 10
+  存储空间: 需要 8 GB 可用空间', 49.9),
+ ('Modern Visual Arts Laboratory', 'Lucy -The Eternity She Wished For', '如果世界上有机器人能够像人一样微笑，一样哭泣，一样悲伤。',
+  '操作系统: Microsoft Windows 2000/XP(SP3)/Vista
+  处理器: Intel Pentium 4 1.5Ghz/AMD Athlon 1800
+  内存: 128 MB RAM
+  DirectX 版本: 9.0c
+  存储空间: 需要 1 GB 可用空间
+  附注事项: Microsoft .Net Framework', 124),
+ ('Klei Entertainment', 'Mark of the Ninja', 'In Mark of the Ninja, you''ll know what it is to truly be a ninja. You must be silent, agile and clever to outwit your opponents in a world of gorgeous scenery and flowing animation. Marked with cursed tattoos giving you heightened senses, every situation presents you with options.',
+  'OS:Windows XP SP3, Windows Vista SP2, Windows 7 SP1
+  Processor:AMD Phenom 9750 or Pentium D 800+
+  Memory:2 GB RAM
+  Graphics:NVIDIA® GeForce® 7800 GT or ATI Radeon™ HD 3000+ or better DirectX®:9.0c
+  Hard Drive:2.5 GB
+  HD space Sound:100% DirectX9.0c compatible sound card and drivers', 66),
+ ('Klei Entertainment', 'Oxygen Not Included', 'Oxygen Not Included is a space-colony simulation game. Deep inside an alien space rock your industrious crew will need to master science, overcome strange new lifeforms, and harness incredible space tech to survive, and possibly, thrive.',
+  '操作系统: Windows 7 (64 bit)
+  处理器: Dual Core 2 GHz
+  内存: 4 GB RAM
+  图形: Intel HD 4600 (AMD or NVIDIA equivalent)
+  DirectX 版本: 11
+  存储空间: 需要 2 GB 可用空间', 76),
+ ('ConcernedApe', 'Stardew Valley', '你继承了你爷爷在星露谷留下的老旧农场。带着爷爷留下的残旧工具和几枚硬币开始了你的新生活。你能适应这小镇上的生活并且将这个杂草丛生的老旧农场变成一个繁荣的家吗？',
+  '操作系统: Windows Vista or greater
+  处理器: 2 Ghz
+  内存: 2 GB RAM
+  图形: 256 mb video memory, shader model 3.0+
+  DirectX 版本: 10
+  存储空间: 需要 500 MB 可用空间', 21),
+ ('Thekla, Inc.', 'The Witness', '你孤身一人醒来。这是一座充满挑战和惊奇、遍地谜题的奇怪小岛。',
+  '操作系统: Windows 7
+  处理器: 1.8GHz
+  内存: 4 GB RAM
+  图形: Intel HD 4000 series
+  DirectX 版本: 10
+  存储空间: 需要 5 GB 可用空间', 21),
+ ('Illusion Ranger', 'Aeon', '我们把所有精力都注入在打击感的挖掘上，创造出一款顶级打击感的VR射击游戏。你将体验大口径火枪爽快的爆裂射击，同时用你的武士刀随心所欲地斩杀胆敢靠近的敌人。',
+  '操作系统: win7
+处理器: intel i5
+内存: 8000 MB RAM
+图形: Nvidia 970
+DirectX 版本: 11
+存储空间: 需要 2000 MB 可用空间
+附注事项: HTC vive only', 55),
+ ('Reactive Drop Team', 'Alien Swarm Reactive Drop', 'Co-operative top-down shooter game available for free. An epic bug hunt featuring a unique blend of co-op play and squad-level tactics.',
+  '操作系统: Windows® XP or above
+处理器: Pentium 4 3.0GHz
+内存: 2 GB RAM
+图形: DirectX 9 compatible video card with 128 MB, Shader model 2.0. ATI X800, NVidia 6600 or better
+DirectX 版本: 9.0
+存储空间: 需要 10 GB 可用空间
+声卡: DirectX 9.0c compatible sound card', 76),
+ ('Valve', 'Counter-Strike Global Offensive', '《反恐精英：全球攻势》（CS: GO）在团队竞技游戏模式的基础上加以开发，这一模式自 12 年前发布以来一直引领至今。 CS: GO 的特色是全新的地图、人物、武器以及最新版本的 CS 经典内容（de_dust 等）。此外，CS: GO 引入了全新的游戏模式、对战匹配、排行榜以及更多新的内容。',
+  '操作系统: Windows® 7/Vista/XP
+处理器: Intel® Core™ 2 Duo E6600 or AMD Phenom™ X3 8750 processor or better
+内存: 2 GB RAM
+图形: Video card must be 256 MB or more and should be a DirectX 9-compatible with support for Pixel Shader 3.0
+DirectX 版本: 9.0c
+存储空间: 需要 15 GB 可用空间', 58),
+ ('Sports Interactive', 'Football Manager 2017', '作为迄今为止最真实的足球经理类游戏，Football Manager 2017 让你操控自己最喜欢的球队征战绿茵赛场。',
+  '操作系统: Windows Vista (SP2), 7 (SP1), 8, 8.1, 10 (1607) – 64-bit or 32-bit
+处理器: Intel Pentium 4, Intel Core or AMD Athlon – 2.2 GHz +
+内存: 2 GB RAM
+图形: Intel GMA X3100, NVIDIA GeForce 8600M GT or AMD/ATI Mobility Radeon HD 2400 – 256MB VRAM
+DirectX 版本: 9.0c
+存储空间: 需要 3 GB 可用空间', 75),
+ ('Telltale Games', 'Marvel''s Guardians of the Galaxy The Telltale Series', '从地球到米兰号再到不毛之地以及更远的地方，伴随着超棒的音乐节奏，这个分为五章节的系列游戏将带你进入穿着火箭靴的星爵的全新Guardians历险。游戏故事由曾获奖的Telltale独特风格讲述，你的选择和动作将决定你所体验的故事轨迹。 ',
+  '操作系统: Windows 7 64Bit Service Pack 1 or higher
+处理器: Intel Core 2 Duo 2.4GHz
+内存: 3 GB RAM
+图形: Nvidia GTS 450+ with 1024MB+ VRAM (excluding GT)
+DirectX 版本: 11
+存储空间: 需要 15 GB 可用空间
+声卡: Direct X 11 sound device', 198),
+ ('Bluehole, Inc.', 'PLAYERUNKNOWN''S BATTLEGROUNDS', '绝地求生(PLAYERUNKNOWN’S BATTLEGROUNDS)是大逃杀类型的游戏，每一局游戏将有100名玩家参与，他们将被投放在绝地岛(battlegrounds)的上空，游戏开始跳伞时所有人都一无所有。',
+  '操作系统: 64-bit Windows 7, Windows 8.1, Windows 10
+处理器: Intel Core i3-4340 / AMD FX-6300
+内存: 6 GB RAM
+图形: nVidia GeForce GTX 660 2GB / AMD Radeon HD 7850 2GB
+DirectX 版本: 11
+网络: 宽带互联网连接
+存储空间: 需要 30 GB 可用空间', 200),
+ ('Keen Games', 'Portal Knights', '将熟悉的世界抛在身后，踏入 Portal Knights 这个奇妙的未知世界！在由古代传送门连接的众多沙盒岛屿上，和你的好友一起展开这场惊险刺激的制作冒险。没有人记得自从和平的世界因为碎裂分崩离析、陷入黑暗之后已经过了多久。是否还有勇者敢于将这片破碎之地复原？打造你的冒险之旅。',
+  '操作系统: 64bit Versions of Windows 7, Windows 8, Windows 10
+处理器: AMD Phenom(tm) 8450 Triple-Core Processor (3 CPUs), ~2.1GHz or Intel Core 2 Duo E8400 @ 3.0 GHz
+内存: 4 GB RAM
+图形: Graphics: NVIDIA® GeForce GTX 470 (1Gb VRAM) / ATI Radeon TM HD 6870 (1Gb VRAM)
+DirectX 版本: 11
+网络: 宽带互联网连接
+存储空间: 需要 2 GB 可用空间
+声卡: On Board', 98),
+ ('Arkane Studios', 'Prey', 'In Prey, you awaken aboard Talos I, a space station orbiting the moon in the year 2032. You are the key subject of an experiment meant to alter humanity forever – but things have gone terribly wrong. The space station has been overrun by hostile aliens and you are now being hunted. ',
+  '操作系统: Windows 7/8/10 (64-bit versions)
+处理器: Intel i5-2400, AMD FX-8320
+内存: 8 GB RAM
+图形: GTX 660 2GB, AMD Radeon 7850 2GB
+存储空间: 需要 20 GB 可用空间', 79),
+ ('Eugen Systems', 'Steel Division Normandy 44', 'Steel Division: Normandy is a tactical real-time strategy (RTS) game that pits players against AI enemies in a single-player campaign -- or against several opponents in massive 10-on-10 multiplayer battles. ',
+  '操作系统: 64-bit Windows 10 / 8.1 / 7 with Service Pack 1
+处理器: Intel Core i3-2100 (3.1 GHz) or equivalent
+内存: 3 GB RAM
+图形: 1 GB AMD 5570 or nVidia 450
+DirectX 版本: 11
+网络: 宽带互联网连接
+存储空间: 需要 32 GB 可用空间
+声卡: DirectX Compatible Sound Card', 66),
+ ('SNK Playmore', 'THE KING OF FIGHTERS XIII STEAM EDITION', '“THE KING OF FIGHTERS XIII”, SNK PLAYMORE’s flagship 2D versus fighting title returns in an ultimate version on Steam!!The many features exclusive to the console version of the game, such as the ONLINE Mode that allows you to enjoy smooth online versus matches with rivals from around the world',
+  '操作系统: Windows XP
+处理器: Intel Pentium4 2.0 GHz and up
+内存: 1 GB RAM
+图形: GeForce 9500 GT ,VRAM: 256MB and up
+DirectX 版本: 9.0c
+存储空间: 需要 5 GB 可用空间
+声卡: DirectSound, DirectX9.0c Compatible Audio', 50),
+  ('Rockstar North', 'GTA Online: Shark Cash Cards', '该内容需要在 Steam 拥有基础游戏 Grand Theft Auto V 才能运行。',
+   '操作系统: Windows 10 64 Bit, Windows 8.1 64 Bit, Windows 8 64 Bit, Windows 7 64 Bit Service Pack 1, Windows Vista 64 Bit Service Pack 2* (*NVIDIA video card recommended if running Vista OS)
+处理器: Intel Core 2 Quad CPU Q6600 @ 2.40GHz (4 CPUs) / AMD Phenom 9850 Quad-Core Processor (4 CPUs) @ 2.5GHz
+内存: 4 GB RAM
+图形: NVIDIA 9800 GT 1GB / AMD HD 4870 1GB (DX 10, 10.1, 11)
+存储空间: 需要 72 GB 可用空间
+声卡: 100% DirectX 10 compatible', 148),
+ ('Giant Squid', 'ABZU', '沿袭 Journey蕴含的艺术思想，ABZU 是一款能唤起潜水梦想的唯美海底冒险游戏。深入海洋中心，将自己沉浸在生机勃勃的隐秘世界中，这里色彩缤纷，生物繁多。但要注意，当你不断深入，危险就潜伏在深处。',
+  '操作系统: Windows 7, 64-bit
+处理器: 3.0GHz CPU Dual Core
+内存: 4 GB RAM
+图形: Geforce GTX 750 / Radeon R7 260X
+DirectX 版本: 11
+存储空间: 需要 6 GB 可用空间
+声卡: DirectX compatible sound card', 68),
+ ('Rad Codex', 'Alvora Tactics', 'Alvora Tactics is an indie Tactics RPG that combines strategic combat with immersive exploration. Explore and conquer Great Serpent Alvora with your own customizable band of warriors!',
+  '操作系统: Windows XP
+内存: 1 GB RAM
+图形: Must support Shader Model 2.0
+DirectX 版本: 9.0c
+存储空间: 需要 1000 MB 可用空间', 32),
+ ('Codemasters', 'DiRT 4', '世界领先的越野赛系列游戏回归了！《DiRT 4》让你驾驶有史以来最强的赛车去面对这个星球的拉力赛、rallycross 和 landrush 中危险重重的赛道。游戏改变了“你的赛段”系统，只要轻轻触摸一个按钮，即可创建无数条赛道。',
+  '操作系统: 64bit Versions of Windows 7, Windows 8, Windows 10
+处理器: AMD FX Series or Intel Core i3 Series
+内存: 4 GB RAM
+图形: AMD HD5570 or NVIDIA GT440 with 1GB of VRAM (DirectX 11 graphics card required)
+网络: 宽带互联网连接
+存储空间: 需要 50 GB 可用空间
+声卡: DirectX Compatible soundcard', 158),
+ ('FromSoftware, Inc.', 'DARK SOULS III', 'Dark Souls即将推出极具话题性及代表性的系列新作。',
+  '操作系统: Windows 7 SP1 64bit, Windows 8.1 64bit Windows 10 64bit
+处理器: Intel Core i3-2100 / AMD® FX-6300
+内存: 4 GB RAM
+图形: NVIDIA® GeForce GTX 750 Ti / ATI Radeon HD 7950
+DirectX 版本: 11
+网络: 宽带互联网连接
+存储空间: 需要 25 GB 可用空间
+声卡: DirectX 11 sound device', 268),
+ ('Laser Unicorns', 'Kung Fury', 'Miami Police Department detective and martial artist Kung Fury time travels from the 1980s to World War II to kill Adolf Hitler, a.k.a. "Kung Führer", and revenge his friend''s death at the hands of the Nazi leader. An error in the time machine sends him further back to the Viking Age.',
+  '操作系统: Windows 7
+处理器: 英特尔酷睿 2 处理器或相同性能的 AMD 处理器
+内存: 1 GB RAM
+网络: 宽带互联网连接
+存储空间: 需要 200 MB 可用空间', 0),
+ ('Rockstar North', 'Grand Theft Auto V', 'A young street hustler, a retired bank robber and a terrifying psychopath must pull off a series of dangerous heists to survive in a ruthless city in which they can trust nobody, least of all each other.',
+  '操作系统: Windows 10 64 Bit, Windows 8.1 64 Bit, Windows 8 64 Bit, Windows 7 64 Bit Service Pack 1, Windows Vista 64 Bit Service Pack 2* (*NVIDIA video card recommended if running Vista OS)
+处理器: Intel Core 2 Quad CPU Q6600 @ 2.40GHz (4 CPUs) / AMD Phenom 9850 Quad-Core Processor (4 CPUs) @ 2.5GHz
+内存: 4 GB RAM
+图形: NVIDIA 9800 GT 1GB / AMD HD 4870 1GB (DX 10, 10.1, 11)
+存储空间: 需要 72 GB 可用空间
+声卡: 100% DirectX 10 compatible', 198),
+ ('IMCGAMES Co.,Ltd.', 'Tree of Savior (English Ver.)', 'Tree of Savior(abbreviated as TOS thereafter) is an MMORPG in which you embark on a journey to search for the goddesses in the world of chaos. Fairy-tale like colors accompanied with beautiful graphics in TOS will have you reminiscing about precious moments all throughout the game.',
+  '操作系统: Windows XP
+处理器: Intel Pentium Core 2
+内存: 4 GB RAM
+图形: NVIDIA Geforce 8600
+DirectX 版本: 9.0c
+网络: 宽带互联网连接
+存储空间: 需要 4 GB 可用空间', 0),
+ ('Daybreak Game Company', 'H1Z1 King of the Kill', '《杀戮之王》是一款单败淘汰制多人在线射击游戏，可容纳150名身经百战的玩家角逐于一个强大而复杂的世界。搜寻武器、弹药、盔甲、车辆和补给，在竞争中脱颖而出，并坚持到最后。',
+  '操作系统: Windows 7, 8, 8.1, 10 (64 bit only)
+处理器: Intel i5 Quad-Core
+内存: 6 GB RAM
+图形: nVidia GeForce GTX 280 series or higher
+DirectX 版本: 10
+网络: 宽带互联网连接
+存储空间: 需要 20 GB 可用空间
+声卡: DirectX Compatible Sound Card', 68),
+ ('Codemasters ,	Feral Interactive (Mac)', 'F1 2016', '在F1 2016中创造您自己的传奇。准备好全身心投入这场有史以来最著名的顶尖赛车赛事中。与您的代理人、工程师与团队共同合作，以十个赛季的丰富经验深度开发您的赛车。',
+  '操作系统: 64bit Versions of Windows 7, Windows 8, Windows 10
+处理器: Intel Core i3 530 or AMD FX 4100
+内存: 8 GB RAM
+图形: Nvidia GTX 460 or AMD HD 5870
+DirectX 版本: 11
+网络: 宽带互联网连接
+存储空间: 需要 30 GB 可用空间
+声卡: DirectX Compatible Soundcard', 158),
+ ('Piranha Bytes', 'ELEX', 'ELEX is a handcrafted action role-playing experience from the award-winning creators of the Gothic series, set in a brand new post-apocalyptic Science Fantasy universe that puts players into a huge seamless game world full of original characters, mutated creatures, deep moral choices and powerful action.',
+  '操作系统: Windows 7, 8, Windows 10 (64 bit)
+处理器: Intel Core i5 at 2.5 GHz or better or AMD Phenom II x4 940 at 3.0 GHz
+内存: 6 GB RAM
+图形: NVIDIA GeForce GTX 570 or AMD Radeon HD 5870, 1 GB VRAM
+DirectX 版本: 11
+存储空间: 需要 20 GB 可用空间
+声卡: DirectX compatible Sound card', 44.1),
+ ('IllFonic', 'Friday the 13th: The Game', 'Jason is back! Jason Voorhees is unleashed and stalking the grounds of Camp Crystal Lake! Friday the 13th: The Game is one of the most highly-anticipated horror titles of all time. You will finally be able to take on the role as Jason Voorhees and Camp Crystal Lake counselors.',
+  '操作系统: Windows 7, Windows 8, Windows 10
+处理器: Intel Core i3-530 (2 * 2930), AMD Athlon II X2 270 (2 * 3400)
+内存: 4096 MB RAM
+图形: GeForce GTX 650 Ti (1024 MB), Radeon HD 7770 (1024 MB)
+DirectX 版本: 11
+网络: 宽带互联网连接
+存储空间: 需要 4 GB 可用空间', 39);
+
+insert into `shop`.`kind`(name) values ('独立'), ('动作'), ('冒险'), ('休闲'), ('策略'), ('模拟'), ('角色扮演'), ('免费'), ('单人'), ('大型多人在线'), ('射击'), ('体育'), ('氛围'), ('暴力');
+
+insert into `shop`.`kindmapper` values (1, 1), (2, 1),(3, 1),(4, 1),(5, 1),(6, 1),(7, 1),(8, 1),(9, 1),(10, 2),(11, 2),(12, 2),(13, 2),(14, 2),(15, 2),(16, 2),(17, 2),(18, 2),(19, 2),(20, 3),(21, 4),(22, 5),(23, 6),(24, 7),(25, 8),(26, 9),(27, 10),(28, 11),(29, 12),(30, 13),(31, 14);
+
+insert into `shop`.`tag`(`name`) values ('模拟'), ('独立'), ('海军'), ('策略'), ('冷战'), ('免费'), ('冒险'), ('角色扮演'), ('日本动画'), ('动作'), ('太空'), ('科幻'), ('单人'), ('剧情'),
+ ('机器人'), ('潜行'), ('忍者'), ('多人'), ('二维'), ('建筑建造'), ('生存'), ('农业'), ('工业'), ('解密'), ('探索'), ('第一人称'), ('射击'), ('虚拟现实'), ('合作'), ('外星人'), ('体育'),
+ ('足球'), ('恐怖'), ('即时战略'), ('街机'), ('格斗'), ('竞技'), ('困难'), ('女主人翁'), ('丧尸'), ('氛围'), ('暴力'), ('犯罪'), ('砍杀'), ('萝莉'), ('养成'), ('种植');
+
+insert into `shop`.`tagmapper` values (
